821be291e3c339870215ceea31d8be157b44be34
JaimeAyoub <ayoubramosjaime@gmail.com>
2026-09-02 18:29:01 -0600

    Tienda

---
+++ Moviles/pwa-tienda/firebase-config.js
@@ -0,0 +1,13 @@
+// pega aqui la configuracion de tu proyecto de Firebase.
+// la consigues en: consola de Firebase -> icono de engranaje ->
+// Configuracion del proyecto -> pestaña "Tus apps" -> SDK setup and configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBy1I_mZQBdin1pKBsTBcX3lZj5uUa-SfU",
+  authDomain: "firsttestproject-153f4.firebaseapp.com",
+  databaseURL: "https://firsttestproject-153f4-default-rtdb.firebaseio.com",
+  projectId: "firsttestproject-153f4",
+  storageBucket: "firsttestproject-153f4.firebasestorage.app",
+  messagingSenderId: "871925718464",
+  appId: "1:871925718464:web:0530985e4fe9f0fa423c25"
+  
+};
